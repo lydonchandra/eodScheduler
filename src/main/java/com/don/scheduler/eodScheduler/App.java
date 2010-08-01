458e7d6f4003fa35fc9a3279c79932409ad253a1
Lydon Chandra <lydonchandra@mini-2.local>
2010-08-02 01:50:07 +0800

    get asx quotes

--- src/main/java/com/don/scheduler/eodScheduler/App.java
+++ src/main/java/com/don/scheduler/eodScheduler/App.java
@@ -1,13 +1,22 @@
 package com.don.scheduler.eodScheduler;
 
+import org.quartz.SchedulerException;
+import org.quartz.impl.StdScheduler;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws SchedulerException
+    {    	
         System.out.println( "Hello World!" );
+        final BeanFactory factory = new XmlBeanFactory(new FileSystemResource("/Users/lydonchandra/Documents/workspace-sts-carbon3/eodScheduler/src/main/resources/eodScheduler.xml"));
+		StdScheduler sched = (StdScheduler)factory.getBean("scheduler");
+		sched.start();
     }
 }
